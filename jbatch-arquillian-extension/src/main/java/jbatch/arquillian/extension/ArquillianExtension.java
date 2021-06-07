9f391f5568a9bfe2790ec1c1f5649ef0e8367150
Ondrej Mihalyi <ondrej.mihalyi@payara.fish>
2021-06-07 02:34:42 +0200

    Runs GlassFish instead of Payara Payara doesn't pass the Jakarta EE 9 TCK yet Signed-off-by: Ondro Mihalyi <ondrej.mihalyi@payara.fish>

--- jbatch-arquillian-extension/src/main/java/jbatch/arquillian/extension/ArquillianExtension.java
+++ jbatch-arquillian-extension/src/main/java/jbatch/arquillian/extension/ArquillianExtension.java
@@ -1,6 +1,6 @@
 package jbatch.arquillian.extension;
 
-import org.jboss.arquillian.container.test.spi.client.deployment.*;
+import org.jboss.arquillian.container.test.spi.client.deployment.DeploymentScenarioGenerator;
 import org.jboss.arquillian.core.spi.LoadableExtension;
 import org.jboss.arquillian.core.spi.LoadableExtension.ExtensionBuilder;
 
